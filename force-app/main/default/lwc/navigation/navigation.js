7ca093771483f902d1c3a5fff9da744de1993569
Arivu-SF <arivarasan.udhayasuriyan@springfive>
2025-04-30 15:00:28 +0530

    Commit

--- force-app/main/default/lwc/navigation/navigation.js
+++ force-app/main/default/lwc/navigation/navigation.js
@@ -4,14 +4,33 @@ import communityBaseUrl from '@salesforce/community/basePath';
 
 export default class Navigation extends LightningElement {
     recordId = 'a0ONS000007lw692AA';   
-    recordName = 'Aswin'; 
+    recordName = 'Aswin Visveswar'; 
     portfolioLink = '';
 
+    homePageUrl = `${communityBaseUrl}`;
+
+        // Hardcoded API Name for Task object and filterName (you can adjust filter as needed)
+        objectApiName = 'Task';  // Hardcoded object API name for Task
+        filterName = 'all';  // You can dynamically change this based on conditions, or hardcode 'all', 'open', etc.
+    
+
+    taskLink = '';
+
+    
     connectedCallback() {
         this.setPortfolioLink();  // Set the portfolio link when the component is loaded
+        this.setTaskLink();  // Set the task link dynamically
     }
         // Construct the portfolio link with recordId and recordName
     setPortfolioLink() {
             this.portfolioLink = `${communityBaseUrl}/portfolio/${this.recordId}/${this.recordName}`;
     }
+
+    // Construct the task link with hardcoded objectApiName and filterName
+    setTaskLink() {
+        this.taskLink = `${communityBaseUrl}/task/${this.objectApiName}/${this.filterName}`;
+    }
+
+
+
 }
